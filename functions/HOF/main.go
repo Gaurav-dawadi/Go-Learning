007fc23e608b622356ac1a13486a542a095f50ba
Gaurav-dawadi <gaurabdawadi75@gmail.com>
2022-05-19 11:09:47 +0545

    feat: add Higher Order Function codes

---
+++ functions/HOF/main.go
@@ -0,0 +1,46 @@
+package main
+import ("fmt")
+
+// Higher Order Function that returns a function
+func doSum() func(x int, y int) int{
+	return func(x int, y int) int{
+		return x+y
+	}
+}
+
+// Higher Order Function that takes variable as argument and returns function
+func findDiff(x int) func(y int) int{
+	return func(y int) int{
+		return x-y
+	}
+}
+
+// Higher Order Function that takes variable as argument and returns function
+func sum(x int, y int) int{
+	return x+y
+}
+func partialSum(x int) func(y int) int{
+	return func(y int) int{
+		return sum(x,y)
+	}
+}
+
+// Higher Order Function that takes function as an argument and returns function
+func cal(a int,b int, performCalculation func(c int,d int) int) int{
+	return performCalculation(a,b)
+}
+
+
+func main(){
+	result_sum := doSum()
+	fmt.Println("The result of sum is ", result_sum(10,20))
+
+	result_partial_sum := partialSum(10)
+	fmt.Println("The result of partial sum is ", result_partial_sum(5))
+
+	result_diff := findDiff(15)
+	fmt.Println("The result of difference is ", result_diff(5))
+
+	result_cal := cal(10,2, func(x int, y int) int {return x+y})
+	fmt.Println("The result of cal is ", result_cal)        // Note: we are not calling result_cal variable
+}
