825649d5f48c593514d14546fa04987e5874d4f7
Gaurav-dawadi <gaurabdawadi75@gmail.com>
2022-05-19 08:41:58 +0545

    refactor: add function that has returns multiple types

--- functions/simple_function_params/main.go
+++ functions/simple_function_params/main.go
@@ -7,11 +7,19 @@ func area_square(l int64, b int64) int64{
 	return area
 }
 
+func calculation(l int64, b int64)(area int64, parameter int64){
+	parameter = 2*(l+b)
+	area = l*b
+	return
+}
+
 func main(){
 	var (
 		length int64 = 10
 		width int64 = 5
 	)
-	result := area_square(length, width)
-	fmt.Println("The area of square is: ", result)
+	result_square := area_square(length, width)
+	result_cal_area,result_cal_param := calculation(length, width)
+	fmt.Println("The area of square is: ", result_square)
+	fmt.Printf("The area is %v and parameter is %v\n", result_cal_area, result_cal_param)
 }
