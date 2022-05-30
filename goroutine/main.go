cdf8655dd663376b39e0bfbaf28e82642d71877b
Gaurav-dawadi <gaurabdawadi75@gmail.com>
2022-05-30 17:45:31 +0545

    feat: add goroutine file

---
+++ goroutine/main.go
@@ -0,0 +1,38 @@
+package main
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+/*
+Goroutines are functions that run independent of each other i.e. concurrently. 
+*/
+func responseSize(url string) {
+	fmt.Println("Step1: ", url)
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Step2: ", url)
+	defer response.Body.Close()
+
+	fmt.Println("Step3: ", url)
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Step4: ", len(body))
+}
+
+func main() {
+	fmt.Println("Start Goroutines")
+	go responseSize("https://www.golangprograms.com")
+	go responseSize("https://stackoverflow.com")
+	go responseSize("https://coderwall.com")
+	time.Sleep(10 * time.Second)
+	fmt.Println("Terminating Program")
+}
